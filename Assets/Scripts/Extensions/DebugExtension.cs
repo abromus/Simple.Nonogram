da7a5ffb6f8e72891aa4f2027145a65bb03a19e0
Roman Dunin <abromus@mail.ru>
2023-01-07 05:43:12 +0300

    Unity расширения

---
+++ Assets/Scripts/Extensions/DebugExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace Simple.Nonogram.Extension
+{
+    public static class DebugExtension
+    {
+        public static void Log(string message, string color = "white")
+        {
+            Debug.Log($"{DateTime.Now}. <color={color}>{message}</color>");
+        }
+
+        public static void Log(string message, Color color)
+        {
+            Log(message, color.ToString());
+        }
+
+        public static void LogWarning(string message, string color = "yellow")
+        {
+            Debug.LogWarning($"{DateTime.Now}. <color={color}>{message}</color>");
+        }
+
+        public static void LogWarning(string message, Color color)
+        {
+            LogWarning(message, color.ToString());
+        }
+
+        public static void LogError(string message, string color = "red")
+        {
+            Debug.LogError($"{DateTime.Now}. <color={color}>{message}</color>");
+        }
+
+        public static void LogError(string message, Color color)
+        {
+            LogError(message, color.ToString());
+        }
+    }
+}
---
+++ Assets/Scripts/Extensions/UnityExtension.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Simple.Nonogram.Extension
+{
+    public static class UnityExtension
+    {
+        public static RectTransform SetSizeWithCurrentAnchors(this RectTransform rectTransform, float width, float height)
+        {
+            var sizeDelta = rectTransform.sizeDelta;
+            var parentSize = GetParentSize(rectTransform);
+
+            sizeDelta.x = Mathf.Abs(width - parentSize.x * (rectTransform.anchorMax.x - rectTransform.anchorMin.x));
+            sizeDelta.y = Mathf.Abs(height - parentSize.y * (rectTransform.anchorMax.y - rectTransform.anchorMin.y));
+            rectTransform.sizeDelta = sizeDelta;
+
+            return rectTransform;
+        }
+
+        private static Vector2 GetParentSize(RectTransform rectTransform)
+        {
+            var rectTransformParent = rectTransform.parent.GetComponent<RectTransform>();
+
+            return rectTransformParent != null ? rectTransformParent.rect.size : Vector2.zero;
+        }
+    }
+}
